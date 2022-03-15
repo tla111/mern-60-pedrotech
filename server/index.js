bae9fa713e0b070f125b73e4d1c105c534c6d7bf
Tim La <tlamarketing111@gmail.com>
2022-03-15 14:03:12 -0400

    created user schema

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://tlala22:Learn2019@cluster0.gpsln.mongodb.net/merntutorial?retryWrites=true&w=majority");
 
 app.listen(3001, () => {
     console.log("SERVER RUNS PERFECTLY!");
