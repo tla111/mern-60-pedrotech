b9237e7a049ed1ae97121287ea5c67a1a631759a
Tim La <tlamarketing111@gmail.com>
2022-03-15 14:41:37 -0400

    able to create user from frontend to backend

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,60 @@ import axios from 'axios';
 
 function App() {
   const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [username, setUsername] = useState("");
+
+
 
   useEffect(() => {
     axios.get("http://localhost:3001/getUsers").then((response) => {
       setListOfUsers(response.data);
-    })
+    });
   }, []);
+
+  const createUser = () => {
+    axios.post("http://localhost:3001/createUser", {
+      name: name,
+      age: age,
+      username: username,
+    }).then((response) => {
+      alert("USER CREATED");
+    });
+  }
 
   return (
     <div className="App">
       <div className="usersDisplay">
         {listOfUsers.map((user) => (
-          <div>
+          <div className="userContainer">
             <h1>Name: {user.name}</h1>
             <h1>Age: {user.age}</h1>
             <h1>Username: {user.username}</h1>
           </div>
         ))}
       </div>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => setName(event.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Age..."
+          onChange={(event) => setAge(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          onChange={(event) => setUsername(event.target.value)}
+
+        />
+        <button onClick={createUser}>Create User</button>
+      </div>
+
     </div>
   );
 }
