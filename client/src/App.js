83a6a6de9abc5e03ab39d1382c6b2a66382ef517
Tim La <tlamarketing111@gmail.com>
2022-03-15 14:28:08 -0400

    able to display sample user

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [listOfUsers, setListOfUsers] = useState([
+    { id: 1, name: "Pedro", age: 20, username: "PedroTech" }
+  ]);
+
   return (
     <div className="App">
-
+      <div className="usersDisplay">
+        {listOfUsers.map((user) => (
+          <div>
+            <h1>Name: {user.name}</h1>
+            <h1>Age: {user.age}</h1>
+            <h1>Username: {user.username}</h1>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
