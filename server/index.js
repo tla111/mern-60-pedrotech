c8df31e1a3cc4d6d7f92ead3c48c30612995d0bb
Tim La <tlamarketing111@gmail.com>
2022-03-15 14:18:31 -0400

    able to create new user

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const UserModel = require("./models/Users");
 
+app.use(express.json());
+
 mongoose.connect("mongodb+srv://tlala22:Learn2019@cluster0.gpsln.mongodb.net/merntutorial?retryWrites=true&w=majority");
 
 app.get("/getUsers", (req, res) => {
@@ -13,6 +15,14 @@ app.get("/getUsers", (req, res) => {
             res.json(result);
         }
     })
+});
+
+app.post("/createUser", async (req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+    res.json(user);
 });
 
 app.listen(3001, () => {
